980d6ca5ea1e8f9b365b4a54fe3e638807ff2b49
Edwin Reyes <reyesedwin2022@gmail.com>
2022-03-24 12:48:46 -0400

    Fetching API

--- api/weather.js
+++ api/weather.js
@@ -4,6 +4,9 @@ export default async function handler(request, res) {
   const { city } = request.query;
 	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.openweathermapappid}`;
 	const currentWeather = await fetch(url).then(res => res.json());
+
+
+  
   res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -11,3 +14,6 @@ export default async function handler(request, res) {
   res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
   res.json(await currentWeather);
 }
+
+
+
--- src/WordGuessingGame.js
+++ src/WordGuessingGame.js
@@ -2,17 +2,68 @@ import { html, css, LitElement } from 'lit';
 import '@lrnwebcomponents/simple-colors';
 
 class WordGuessingGame extends LitElement {
+  static get tag() {
+    return 'word-guess-game';
+  }
+
   constructor() {
     super();
-    this.count = 10;
+    this.count = 100;
+    this.seed = null;
+    this.wordArr = [];
   }
 
   static get properties() {
     return {
-      count: {
-        type: Number,
-        reflect: true,
-      },
+      count: { type: String, reflect: true },
+      seed: { type: String },
+      wordArr: {type: Array},
     };
   }
+
+  firstUpdated() {
+    if (this.seed === null) {
+      this.seed = Math.random().toString(36).substring(2, 12);
+    }
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === 'count' && this[propName]) {
+        this.getData();
+      } else if (propName === 'seed' && this[propName]) {
+        let seed = 1;
+        for (let i = 0; i < this.seed.length; i++) {
+          seed *= this.seed.charCodeAt(i);
+        }
+        seed = seed.toString().substring(1, 4);
+        console.log(seed);
+        console.log(this.wordArr[seed]);
+      }
+    });
+  }
+
+
+
+  async getData() {
+    return fetch(`https://random-word-api.herokuapp.com/all`)
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        return false;
+      })
+      .then(data => {
+        this.wordArr = data.filter(item => item.length === 5);
+        let today = new Date().toISOString().slice(0, 10);
+        this.seed = today;
+        return data;
+      });
+  }
+
+  render() {
+    return html``;
+  }
 }
+
+customElements.define(WordGuessingGame.tag, WordGuessingGame);
